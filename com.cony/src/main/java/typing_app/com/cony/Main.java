699a8bc001e6afac3411179a9514520d9a239e30
cony <cony@cony>
2020-02-24 21:05:14 +0900

    ok

--- com.cony/src/main/java/typing_app/com/cony/Main.java
+++ com.cony/src/main/java/typing_app/com/cony/Main.java
@@ -10,10 +10,7 @@ public class Main {
 				System.exit(0);
 			}
 		});
-<<<<<<< HEAD
 		System.out.println("main update conflict here");
-=======
 		System.out.println("conflict here");
->>>>>>> refs/heads/create_conflict
 	}
 }
