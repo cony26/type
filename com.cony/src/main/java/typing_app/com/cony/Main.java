371fd0897122b294942f8ee3219416a3547a65d7
cony <cony@cony>
2020-02-25 21:54:26 +0900

    1

--- com.cony/src/main/java/typing_app/com/cony/Main.java
+++ com.cony/src/main/java/typing_app/com/cony/Main.java
@@ -7,6 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		(new Game("Typing Game")).addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
+				System.out.println("branch2 - 1");
 				System.exit(0);
 			}
 		});
